5ca73e9dc496323abd0c9003724086490941363c
NHutt <a1704562@myy.haaga-helia.fi>
2019-01-31 13:49:29 +0200

    connect lisätty

---
+++ connect.php
@@ -0,0 +1,17 @@
+
+<?php
+function connect_db() {
+ $servername = "localhost";
+ $username = "a1704562";
+ $password = "raNAdj87c";
+ $dbname = "a1704562";
+ // Create connection
+ $conn = new mysqli($servername, $username, $password, $dbname);
+ // Check connection
+ if ($conn->connect_error) {
+ die("Connection failed: " . $conn->connect_error);
+ } else {
+ }
+ return $conn;
+}
+?>
