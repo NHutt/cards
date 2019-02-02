850b6c90f5f8d0e1a1572d7dd9f37bb5c3b326e9
NHutt <a1704562@myy.haaga-helia.fi>
2019-02-02 20:23:38 +0200

    config

--- connect.php
+++ connect.php
@@ -1,12 +1,8 @@
 
 <?php
 function connect_db() {
- $servername = "localhost";
- $username = "a1704562";
- $password = "raNAdj87c";
- $dbname = "a1704562";
  // Create connection
- $conn = new mysqli($servername, $username, $password, $dbname);
+ $conn = new mysqli();
  // Check connection
  if ($conn->connect_error) {
  die("Connection failed: " . $conn->connect_error);
@@ -15,3 +11,4 @@ function connect_db() {
  return $conn;
 }
 ?>
+
